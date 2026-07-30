2e955924435832d357a948a858facd15f1b899e4
swirlsocietytt <swirlsocietytt@gmail.com>
2026-07-30 13:30:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ const cartContainer = document.getElementById("cart");
 const cartTotal = document.getElementById("cart-total");
 
 
+
 buttons.forEach(button => {
+
 
     button.addEventListener("click", () => {
 
@@ -21,26 +23,51 @@ buttons.forEach(button => {
         const productName = productCard.querySelector("h3").innerText;
 
 
-        const productPrice = Number(
-            productCard.querySelector("strong").innerText.replace("$","")
-        );
+        let price;
+
+
+        const option = productCard.querySelector(".product-option");
+
+
+        if (option) {
+
+            price = Number(option.value);
+
+        } else {
+
+            price = Number(
+                productCard.querySelector("strong")
+                .innerText
+                .replace("$","")
+            );
+
+        }
+
 
 
         cart.push({
+
             name: productName,
-            price: productPrice
+
+            price: price
+
         });
+
 
 
         updateCart();
 
+
     });
+
 
 });
 
 
 
+
 function updateCart(){
+
 
     cartContainer.innerHTML = "";
 
@@ -48,35 +75,53 @@ function updateCart(){
     let total = 0;
 
 
+
     cart.forEach(item => {
+
 
         total += item.price;
 
 
+
         let div = document.createElement("div");
+
 
         div.classList.add("cart-item");
 
 
+
         div.innerHTML = `
+
         <span>${item.name}</span>
+
         <span>$${item.price}</span>
+
         `;
+
 
 
         cartContainer.appendChild(div);
 
+
+
     });
+
 
 
     if(cart.length === 0){
 
+
         cartContainer.innerHTML =
         "<p>Your cart is currently empty.</p>";
+
 
     }
 
 
-    cartTotal.innerText = "Total: $" + total;
+
+    cartTotal.innerText =
+    "Total: $" + total;
+
+
 
 }
