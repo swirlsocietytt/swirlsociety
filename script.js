a18066d707be8254581b97e421287ea17692dbfe
swirlsocietytt <swirlsocietytt@gmail.com>
2026-07-30 12:36:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,25 @@ let cart = [];
 
 const buttons = document.querySelectorAll(".add-to-cart");
 
+const cartContainer = document.getElementById("cart");
+
+const cartTotal = document.getElementById("cart-total");
+
 
 buttons.forEach(button => {
 
     button.addEventListener("click", () => {
 
+
         const productCard = button.parentElement;
+
 
         const productName = productCard.querySelector("h3").innerText;
 
-        const productPrice = productCard.querySelector("strong").innerText;
+
+        const productPrice = Number(
+            productCard.querySelector("strong").innerText.replace("$","")
+        );
 
 
         cart.push({
@@ -23,10 +32,51 @@ buttons.forEach(button => {
         });
 
 
-        alert(productName + " added to your order!");
-
-        console.log(cart);
+        updateCart();
 
     });
 
 });
+
+
+
+function updateCart(){
+
+    cartContainer.innerHTML = "";
+
+
+    let total = 0;
+
+
+    cart.forEach(item => {
+
+        total += item.price;
+
+
+        let div = document.createElement("div");
+
+        div.classList.add("cart-item");
+
+
+        div.innerHTML = `
+        <span>${item.name}</span>
+        <span>$${item.price}</span>
+        `;
+
+
+        cartContainer.appendChild(div);
+
+    });
+
+
+    if(cart.length === 0){
+
+        cartContainer.innerHTML =
+        "<p>Your cart is currently empty.</p>";
+
+    }
+
+
+    cartTotal.innerText = "Total: $" + total;
+
+}
